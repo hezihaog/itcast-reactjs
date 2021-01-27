209a301f44ef5a18e688550a8fd7b339a693e327
zihe <he100zihao100@126.com>
2021-01-27 17:34:15 +0800

    add: 增加左侧的侧边栏，增加2个Tab，添加用户、用户列表的跳转

---
+++ src/pages/user/UserList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class UserList extends React.Component {
+    render() {
+        return (
+            <div>用户列表</div>
+        );
+    }
+}
+
+export default UserList;
